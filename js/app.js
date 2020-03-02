8260b824d19a5f5accc8003beed3df767ca1c77e
Артур <arthur.betsa@gmail.com>
2020-03-02 23:23:03 +0200

    js created

--- js/app.js
+++ js/app.js
@@ -1,23 +1,60 @@
 $(function () {
 
+
+    // filter
     let filter = $("[data-filter]");
     filter.on("click", function (event) {
         event.preventDefault();
 
         let cat = $(this).data('filter');
 
-        if(cat == "All"){
+        if (cat == "All") {
             $("[data-cat]").removeClass("hide");
-        }else {
+        } else {
             $("[data-cat]").each(function () {
                 let workCat = $(this).data('cat');
 
                 if (workCat != cat) {
                     $(this).addClass('hide');
-                }else {
+                } else {
                     $(this).removeClass("hide");
                 }
             });
         }
-    })
+    });
+
+
+    // modal
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function (event) {
+        event.preventDefault();
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $('body').addClass("no-scroll");
+
+    });
+
+    modalClose.on("click", function (event) {
+        event.preventDefault();
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.removeClass('show');
+        $('body').removeClass("no-scroll");
+
+  // 11:00
+    });
+
+    // slider: https://kenwheeler.github.io/slick/
+
+    $('#works-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true
+    });
 });
