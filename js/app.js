ae4be6047d349b264376176010f4a5b8acad33aa
Артур <arthur.betsa@gmail.com>
2020-03-10 15:40:43 +0200

    slider

--- js/app.js
+++ js/app.js
@@ -36,6 +36,8 @@ $(function () {
         $(modalId).addClass('show');
         $('body').addClass("no-scroll");
 
+        $("#works-slider").slick('setPosition');
+
     });
 
     modalClose.on("click", function (event) {
@@ -46,15 +48,36 @@ $(function () {
         modalParent.removeClass('show');
         $('body').removeClass("no-scroll");
 
-  // 11:00
     });
 
+    $(".modal").on("click", function (event) {
+        $(this).removeClass("show");
+        $("body").removeClass("no-scroll");
+    });
+//почему не работает стрелочная функция тут?
+// Почему разный контекст при function (event){}  И event => {} ???
+
+//     $(".modal").on("click", event => {
+//         $(this).removeClass("show");
+//         $("body").removeClass("no-scroll");
+//     });
+
+
+    //но работает тут?
+    $(".modal__dialog").on("click", event => {
+        event.stopPropagation();
+    });
+
+    // ===========================================
     // slider: https://kenwheeler.github.io/slick/
+    // ===========================================
 
     $('#works-slider').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        fade: true
+        fade: true,
+        arrows: false,
+        dots: false
     });
 });
